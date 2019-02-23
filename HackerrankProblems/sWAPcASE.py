a3d347d9ca7baa5a887898f3ad77fa076af535fb
jamiul <jamiulewu24@gmail.com>
2019-02-23 13:43:51 +0600

    swapCase

--- HackerrankProblems/sWAPcASE.py
+++ HackerrankProblems/sWAPcASE.py
@@ -19,9 +19,9 @@ Str1=input("Enter the string to be converted lowercase: ")
 
 for i in range (0,len(Str1)):
 
-   x=ord(Str1[i])
+   x=ord(Str1[i]) # The ord() method returns an integer representing the Unicode code point of the given Unicode character.Ex 65 = A
    if x>=65 and x<=90: # if x>=97 and x <=122
        x=x+32 # x=x-32
-   y=chr(x)
+   y=chr(x) # The chr() method is the inverse of ord()
    print(y,end="")
    '''
